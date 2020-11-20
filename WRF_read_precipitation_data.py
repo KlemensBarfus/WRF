e9d745713cefa7cbc6a73f039a8542e301dd0778
KlemensBarfus <klemens.barfus@gmx.de>
2020-11-20 23:15:55 +0100

    Update WRF_read_precipitation_data.py

--- WRF_read_precipitation_data.py
+++ WRF_read_precipitation_data.py
@@ -2,6 +2,7 @@ def WRF_read_precipitation_data(file_wrf):
   # reads WRF precipitation data
   # file_wrf is either a file handle or a filename
   from netCDF4 import Dataset
+  import numpy as np
   if(type(file_wrf) is str):
     f_wrf = Dataset(file_wrf, "r", format="NetCDF4")
   else:
