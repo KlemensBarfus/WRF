956417cf26216d5d8c8a6a559bc6dc33bd820820
KlemensBarfus <klemens.barfus@gmx.de>
2020-11-20 18:25:45 +0100

    Update WRF_routines.py

--- WRF_routines.py
+++ WRF_routines.py
@@ -26,14 +26,7 @@ def WRF_filename_from_date(date, n_domain):
   minute = str(date.minute).zfill(2)
   second = str(date.second).zfill(2)
   str1 = "wrf_d"+str(n_domain).zfill(2)+"_"
-  str2 = year+"-"+month+"-"+day+"_"
-  str3 = hour+":"+minute+":"+second
-  res_filename = str1+str2+str3
-  return res_filename
-
-def deaccumulate_wrf_precipitation_data(wrf_data):
-  # deaccumulates WRF precipitation data and transfers it to mm/h
-  import numpy
+  str2 = year+"-"+month+"-"+day+"_"def deaccumulate_wrf_precipitation_data(wrf_data, ref_data=None):
   nxy = wrf_data.shape
   wrf_data_new = np.copy(wrf_data)
   n_times = nxy[0]
@@ -41,8 +34,33 @@ def deaccumulate_wrf_precipitation_data(wrf_data):
   nx = nxy[2]
   for i in range(0, n_times):
     if(i == 0):
-      temp = wrf_data[i,:,:]
+      if(ref_data is not None):
+        temp = wrf_data[i,:,:] - ref_data
+      else:
+        temp = wrf_data[i,:,:]
     else:
       temp = wrf_data[i,:,:] - wrf_data[i-1,:,:]
     wrf_data_new[i,:,:] = temp * 12 # mm to mm/h                                                                                                                                                            
   return wrf_data_new
+  str3 = hour+":"+minute+":"+second
+  res_filename = str1+str2+str3
+  return res_filename
+
+def WRF_deaccumulate_precipitation_data(wrf_data, ref_data=None):
+  nxy = wrf_data.shape
+  wrf_data_new = np.copy(wrf_data)
+  n_times = nxy[0]
+  ny = nxy[1]
+  nx = nxy[2]
+  for i in range(0, n_times):
+    if(i == 0):
+      if(ref_data is not None):
+        temp = wrf_data[i,:,:] - ref_data
+      else:
+        temp = wrf_data[i,:,:]
+    else:
+      temp = wrf_data[i,:,:] - wrf_data[i-1,:,:]
+    wrf_data_new[i,:,:] = temp * 12 # mm to mm/h                                                                                                                                                            
+  return wrf_data_new
+
+
