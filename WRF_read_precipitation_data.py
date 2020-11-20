254c0d28f19be02ca8fd0f409b05b476d7be3fa2
KlemensBarfus <klemens.barfus@gmx.de>
2020-11-20 22:24:36 +0100

    Add files via upload

---
+++ WRF_read_precipitation_data.py
@@ -0,0 +1,14 @@
+def WRF_read_precipitation_data(file_wrf):
+  # reads WRF precipitation data
+  # file_wrf is either a file handle or a filename
+  from netCDF4 import Dataset
+  if(type(file_wrf) is str):
+    f_wrf = Dataset(filename_wrf, "r", format="NetCDF4")
+  else:
+    f_wrf = file_wrf
+  rainc = f_wrf.variables["RAINC"][:]
+  rainnc = f_wrf.variables["RAINNC"][:]
+  rain = np.add(rainc,rainnc)
+  if(type(file_wrf) is str):
+    f_wrf.close()
+  return rain
