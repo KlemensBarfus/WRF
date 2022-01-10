66475ae44ffd84c3199ee44df2dd434e69cbbc37
KlemensBarfus <klemens.barfus@gmx.de>
2022-01-10 11:44:39 +0100

    Add files via upload
    The flag 'rst_inname' for WRF restart run does not work properly (at least for WRF4.2.1). To circumvent this problem, this Python script will read the folder for the restart files from 'rst_outname' in namelist.input and will set links to the restart files in the directory where WRF is started from. It needs to be run from the directory where WRF is started from as "python3 link_restart_files.py"

---
+++ link_restart_files.py
@@ -0,0 +1,29 @@
+# links the restart files to the directory where WRF runs
+# directory where restart files are stored is read from namelist.input variable 
+
+import os
+import glob
+
+filename_namelist = "namelist.input"
+f_in = open(filename_namelist, "rt")
+found = False
+while found == False:
+  str1 = f_in.readline()
+  if("rst_outname" in str1):
+    found = True
+    str1 = str1.rstrip() # remove \n from string
+    i = str1.find("=")
+    str2 = str1[i+1:]
+    ii = [i for i, x in enumerate(str2) if (x == '"' or x == "'")]
+    str3 = str2[ii[0]+1:ii[1]]
+    ii = str3.rfind("/")
+    path_files = str3[0:ii+1]      
+f_in.close()  
+
+# get all restart files in the directory
+wrfrst_files = glob.glob(path_files+"wrfrst_d*")
+for wrfrst_file in wrfrst_files:
+  sys_cmd = "ln -sf "+wrfrst_file+" "+wrfrst_file[ii+1:] 
+  os.system(sys_cmd)
+
+
