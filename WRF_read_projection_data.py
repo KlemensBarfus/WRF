a953b4038569b2ff4b3371362b97e4011ec4c5c1
KlemensBarfus <klemens.barfus@gmx.de>
2022-03-17 11:07:49 +0100

    Add files via upload

---
+++ WRF_read_projection_data.py
@@ -0,0 +1,15 @@
+def WRF_read_projection_data(filename):
+  import numpy as np  
+  from netCDF4 import Dataset
+  f = Dataset(filename, "r", format="NetCDF4")
+  center_latitude = f.getncattr('CEN_LAT')
+  center_longitude = f.getncattr('CEN_LON')
+  truelat1 = f.getncattr('TRUELAT1')
+  truelat2 = f.getncattr('TRUELAT2')
+  standard_longitude = f.getncattr('STAND_LON')
+  map_projection = f.getncattr('MAP_PROJ_CHAR')
+  f.close()
+  proj = {'center_latitude': center_latitude, 'center_longitude': center_longitude, 'truelat1': truelat1, 'truelat2': truelat2, \
+          'standard_longitude': standard_longitude, 'map_projection': map_projection}
+  return proj
+  
