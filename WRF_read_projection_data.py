b0673a7241ace695530d5a6cdf5d114a2f6558fe
KlemensBarfus <klemens.barfus@gmx.de>
2022-03-17 12:20:26 +0100

    Update WRF_read_projection_data.py
    Update refers to the case where global attribute 'MAP_PROJ_CHAR' does not exist but attribute 'MAP_PROJ'.

--- WRF_read_projection_data.py
+++ WRF_read_projection_data.py
@@ -5,9 +5,16 @@ def WRF_read_projection_data(filename):
   center_latitude = f.getncattr('CEN_LAT')
   center_longitude = f.getncattr('CEN_LON')
   truelat1 = f.getncattr('TRUELAT1')
-  truelat2 = f.getncattr('TRUELAT2')
+  truelat2 = f.getncattr('TRUELAT2')attribute_names = []
   standard_longitude = f.getncattr('STAND_LON')
-  map_projection = f.getncattr('MAP_PROJ_CHAR')
+  for name in f.ncattrs():
+    attribute_names.append(name)
+  if('MAP_PROJ_CHAR' in attribute_names):
+    map_projection = f.getncattr('MAP_PROJ_CHAR')
+  else:
+    map_projection_nr = f.getncattr('MAP_PROJ')
+    if(map_projection_nr == 1):
+      map_projection = 'Lambert Conformal'
   f.close()
   proj = {'center_latitude': center_latitude, 'center_longitude': center_longitude, 'truelat1': truelat1, 'truelat2': truelat2, \
           'standard_longitude': standard_longitude, 'map_projection': map_projection}
