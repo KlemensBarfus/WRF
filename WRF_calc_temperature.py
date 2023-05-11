0bd902d0dad9d7da3978fbdcbcb785e36c0a2f48
KlemensBarfus <klemens.barfus@gmx.de>
2023-05-11 13:18:28 +0200

    Update WRF_calc_temperature.py

--- WRF_calc_temperature.py
+++ WRF_calc_temperature.py
@@ -12,12 +12,12 @@ def WRF_calc_temperature(theta,P):
     P = P[None]
     scalarinput = True
 
-    Rd_Cp   = 0.28571 #(dimensionless)
-    T =  theta*((P/100000. )**(Rd_Cp))
+  Rd_Cp = 0.28571 #(dimensionless)
+  T =  theta*((P/100000. )**(Rd_Cp))
 
-    if scalar_input:
-      return np.squeeze(T)
-    return T
+  if scalar_input:
+    return np.squeeze(T)
+  return T
 
 
 
